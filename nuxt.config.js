1b85c39ca4d6bbff52e4d89c430bb26686a4dc83
mktcode <kontakt@markus-kottlaender.de>
2018-09-10 15:10:21 +0200

    implemented youtube embed navbar

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    {src: '~/plugins/vue-youtube-embed.js', ssr: false}
   ],
 
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <section class="container">
-    <h1>Hof Pente</h1>
-  </section>
+  <div class="h-100">
+    <Navbar />
+    <iframe
+      id="youtube-player-1"
+      frameborder="0"
+      allowfullscreen="1"
+      allow="autoplay; encrypted-media"
+      title="YouTube video player"
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/5VZj8U4vP3s?autoplay=0&time=0&enablejsapi=1&origin=http://localhost:3000&widgetid=1&showinfo=0&controls=0&theme=light&color=white"
+    ></iframe>
+  </div>
 </template>
+
+<script>
+  import Navbar from '~/components/Navbar'
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
