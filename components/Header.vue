209afff6a205338a6764fcd83f9a443d064ad0b4
mktcode <kontakt@markus-kottlaender.de>
2018-09-10 20:34:59 +0200

    moved header in own component

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div>
+      <div class="responsive-video">
+        <img src="/logo.png" alt="Hof Pente" class="video-logo" />
+        <iframe
+          width="100%"
+          src="https://www.youtube.com/embed/5VZj8U4vP3s?autoplay=0&time=0&enablejsapi=1&origin=http://localhost:3000&widgetid=1&showinfo=0&theme=light&color=white"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="sass">
+  @import '../assets/css/variables'
+
+  header
+    background-color: $brand-color
+    > div
+      max-width: 800px
+      margin: 0 auto
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,7 @@
   <div class="h-100">
     <Navbar />
 
-    <div class="bg-brand">
-      <div style="max-width: 800px; margin: 0 auto;">
-        <div class="responsive-video">
-          <img src="/logo.png" alt="Hof Pente" class="video-logo" />
-          <iframe
-            width="100%"
-            src="https://www.youtube.com/embed/5VZj8U4vP3s?autoplay=0&time=0&enablejsapi=1&origin=http://localhost:3000&widgetid=1&showinfo=0&theme=light&color=white"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div>
-    </div>
+    <Header />
 
     <div class="container">
       <h1 class="text-center mt-5">Crowdfunding Hof Pente</h1>
@@ -69,11 +57,13 @@
 
 <script>
   import Navbar from '~/components/Navbar'
+  import Header from '~/components/Header'
   import Footer from '~/components/Footer'
 
   export default {
     components: {
       Navbar,
+      Header,
       Footer
     },
     computed: {
