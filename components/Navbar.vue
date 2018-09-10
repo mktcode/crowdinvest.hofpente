85945d4ed95934a561b5a95466d55316c0c66fd6
mktcode <kontakt@markus-kottlaender.de>
2018-09-10 21:07:14 +0200

    navbar box shadow

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarContent">
       <ul class="navbar-nav mx-auto">
         <li class="nav-item">
           <a class="nav-link" href="#">Die Situation</a>
@@ -22,3 +22,8 @@
     </div>
   </nav>
 </template>
+
+<style lang="sass">
+  .navbar
+    box-shadow: 0 0 25px rgba(0, 0, 0, 0.1)
+</style>
