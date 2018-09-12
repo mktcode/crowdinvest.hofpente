2dfe758dc410ebd5fd2f0352e21fe2d41effd779
mktcode <kontakt@markus-kottlaender.de>
2018-09-12 13:42:01 +0200

    added success/error messages on admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -7,6 +7,21 @@
       Key:
       <input type="password" class="form-control" v-model="key" />
 
+      <div class="alert alert-success mt-3" v-if="showSuccessMessage">
+        <button type="button" class="close" aria-label="Close" @click="showSuccessMessage = false">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h5>Daten Erfolgreich aktualisiert!</h5>
+        Lade die Crowdfunding Seite neu, um die Änderungen zu sehen.
+      </div>
+      <div class="alert alert-danger mt-3" v-if="showErrorMessage">
+        <button type="button" class="close" aria-label="Close" @click="showErrorMessage = false">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h5>Fehler!</h5>
+        Das hat leider nicht geklappt. Hast du den richtigen Key eingegeben?
+      </div>
+
       <div class="text-right my-3">
         <button @click.prevent="sendData" class="btn btn-brand" v-if="isValidJson">
           <i class="fas fa-spinner fa-spin" v-if="loading"></i>
@@ -28,7 +43,9 @@
       return {
         key: '',
         json: '',
-        loading: false
+        loading: false,
+        showSuccessMessage: false,
+        showErrorMessage: false
       }
     },
     computed: {
@@ -47,8 +64,10 @@
         this.loading = true
         axios.post('/api/data', {data: JSON.parse(this.json), key: this.key}).then(() => {
           this.loading = false
+          this.showSuccessMessage = true
         }).catch(() => {
           this.loading = false
+          this.showErrorMessage = true
         })
       }
     },
