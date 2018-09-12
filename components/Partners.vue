dd41fe260305075867a349c75f2811f479a50779
mktcode <kontakt@markus-kottlaender.de>
2018-09-12 10:37:11 +0200

    linked partners

--- components/Partners.vue
+++ components/Partners.vue
@@ -2,52 +2,52 @@
   <div class="container py-5 partners">
     <div class="row">
       <div class="col text-center">
-        <a href="#">
+        <a href="http://www.abl-ev.de/start/" target="_blank">
           <img src="/partners/abl.jpg" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://www.akademiefuerpotentialentfaltung.org/" target="_blank">
           <img src="/partners/akademie-potential.png" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://baglob.de/" target="_blank">
           <img src="/partners/baglob.png" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://www.demeter.de/" target="_blank">
           <img src="/partners/demeter.png" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://www.oekolandbau.de/verbraucher/demonstrationsbetriebe/" target="_blank">
           <img src="/partners/demonstrationsbetriebe-oekolandbau.jpg" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://www.landvolk.net/" target="_blank">
           <img src="/partners/lv.jpg" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="http://www.monsanto-tribunald.org/" target="_blank">
           <img src="/partners/monsanto-tribunal.png" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="https://www.solidarische-landwirtschaft.org" target="_blank">
           <img src="/partners/solawi.jpg" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="http://www.handlungspaedagogik.org/" target="_blank">
           <img src="/partners/ag-hp.jpg" />
         </a>
       </div>
       <div class="col text-center">
-        <a href="#">
+        <a href="http://www.nascent-transformativ.de/" target="_blank">
           <img src="/partners/nascent.jpg" />
         </a>
       </div>
